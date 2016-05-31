cec19c0b672ec2467beb93c29514f74c57defb5e
lucasmro <lucasmro@gmail.com>
2016-05-31 20:06:18 -0300

    Run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430173853) do
+ActiveRecord::Schema.define(version: 20160531225657) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -21,9 +21,26 @@ ActiveRecord::Schema.define(version: 20160430173853) do
     t.string   "area"
     t.string   "email"
     t.string   "phone"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
+
+  add_index "companies", ["confirmation_token"], name: "index_companies_on_confirmation_token", unique: true
+  add_index "companies", ["email"], name: "index_companies_on_email", unique: true
+  add_index "companies", ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
 
   create_table "employees", force: :cascade do |t|
     t.string   "cpf"
