e815d4ad877d1430f85c4423a9e8f1a382642ad2
lucasmro <lucasmro@gmail.com>
2016-06-05 09:57:45 -0300

    Restrict access to feedbacks and employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,6 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_company!
 
   # GET /employees
   # GET /employees.json
--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,6 @@
 class FeedbacksController < ApplicationController
   before_action :set_feedback, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_company!
 
   # GET /feedbacks
   # GET /feedbacks.json
