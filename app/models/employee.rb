0afc6832b7f7ad8a496b610793710d2aa9e44de3
lucasmro <lucasmro@gmail.com>
2016-06-10 09:48:36 -0300

    Add validate uniqueness cpf (employee)

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,5 +4,13 @@ class Employee < ActiveRecord::Base
   validates :cpf, presence: true
   usar_como_cpf :cpf
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  validate :cpf_unique
+
+  def cpf_unique
+    if self.cpf and !self.cpf.to_s.empty? and Employee.where(cpf: self.cpf).where('id <> ?', self.id || 0).first
+      errors.add(:cpf, "já está cadastrado")
+    end
+  end
 end
