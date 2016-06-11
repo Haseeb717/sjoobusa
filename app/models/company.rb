d35b0e2001ff7bfeecbf0113f0281d72107ae41b
lucasmro <lucasmro@gmail.com>
2016-06-11 09:17:58 -0300

    Add validate uniqueness cnpj (company)

--- app/models/company.rb
+++ app/models/company.rb
@@ -9,4 +9,12 @@ class Company < ActiveRecord::Base
   usar_como_cnpj :cnpj
   validates :name, presence: true
   validates :trade, presence: true
+
+  validate :cnpj_unique
+
+  def cnpj_unique
+    if self.cnpj and !self.cnpj.to_s.empty? and Company.where(cnpj: self.cnpj).where('id <> ?', self.id || 0).first
+      errors.add(:cnpj, "já está cadastrado")
+    end
+  end
 end
