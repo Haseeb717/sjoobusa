d305d6c106b79a940fb0f00e20663d85e76eb446
lucasmro <lucasmro@gmail.com>
2016-06-20 22:13:19 -0300

    Run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621002850) do
+ActiveRecord::Schema.define(version: 20160621011217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160621002850) do
     t.integer  "late_for_work"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.integer  "termination_reason"
   end
 
   add_index "feedbacks", ["company_id"], name: "index_feedbacks_on_company_id", using: :btree
